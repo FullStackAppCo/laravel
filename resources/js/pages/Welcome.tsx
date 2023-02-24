449f5ca1956621449d73a09d49ae1503268d1d19
Joby Harding <code@fullstackapp.co>
2023-02-24 16:50:00 +0000

    Add Font Awesome

--- resources/js/pages/Welcome.tsx
+++ resources/js/pages/Welcome.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import Logo from '../components/Logo'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceGrinStars } from '@fortawesome/free-regular-svg-icons/faFaceGrinStars'
 
 export default function Welcome() {
     return (
         <div className="p-10 space-y-6">
             <Logo />
             <h1 className="text-xl">
-                Let's build something rad 😎 with Laravel!
+                Let's build a rad web app{' '}
+                <FontAwesomeIcon icon={faFaceGrinStars} /> with Laravel!
             </h1>
         </div>
     )
